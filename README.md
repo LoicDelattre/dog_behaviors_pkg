37f6457c9174fc44fdac57ea934ff23cf6298438
Loic Delattre <loic.delattre@ecam.fr>
2025-03-25 14:14:51 +0100

    readme with instructions

---
+++ README.md
@@ -0,0 +1,12 @@
+###on dog robot###
+First run in seperate terminals:
+
+**For publishing images to pc (debug)**
+$python3 image_publisher.py 
+
+**For playing sounds**
+$python3 sound_publisher.py
+#GoForBall publishes to it, and it then publishes to sound_play
+
+**For going for Ball**
+$python3 GoForBall.py
--- src/SoundPlayer.py
+++ src/SoundPlayer.py
@@ -14,9 +14,13 @@ def sound_callback(msg):
         pass
 
 def main():
+    rospy.init_node("sound_publisher", anonymous=True)
+
+    # Create a publisher (does not send data yet)
+    pub = rospy.Publisher("/sound_type", String, queue_size=10)
+
     rospy.init_node('sound_player', anonymous=True)
 
-    subscriber = rospy.Subscriber("/sound_type", String, sound_callback)
     
     rospy.spin()
     
